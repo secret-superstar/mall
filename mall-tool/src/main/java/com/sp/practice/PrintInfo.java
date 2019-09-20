ccee27d27b2b3bc748651cce011428d66d4da487
phil123 <shupan@kuaizi.co>
2019-09-20 23:13:06 +0800

    aaa

---
+++ mall-tool/src/main/java/com/sp/practice/PrintInfo.java
@@ -0,0 +1,15 @@
+package com.sp.practice;
+
+/**
+ * Created by Macx on 2019/9/20.
+ */
+public class PrintInfo {
+
+    public static void print(String str) {
+        System.out.println(str);
+    }
+
+    public static void println(String str) {
+        System.out.println(str);
+    }
+}
